67fb253b6768ed0573965b4e0dd2fd556d632407
liboxxa <2583036483@qq.com>
2025-11-30 11:07:47 +0800

    切换后端到 Zeabur

--- travel-client/src/config/api.js
+++ travel-client/src/config/api.js
@@ -4,7 +4,7 @@ const DEV_IP = '192.168.1.31';
 const PORT = '5000';
 
 // 基础 URL
-export const API_BASE_URL = 'https://travel-app-pi-ruddy.vercel.app';
+export const API_BASE_URL = 'https://travel-server.zeabur.app';
 
 // 接口路径常量
 export const ENDPOINTS = {
